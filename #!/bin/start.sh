fd658abbbcb777601fda8e5b60acab211c22cdb3
Emman <101398596+blackyaksha@users.noreply.github.com>
2025-04-01 21:21:57 +0800

    Update start.sh

--- #!/bin/start.sh
+++ #!/bin/start.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-# Install Chrome & Chromedriver
+echo "Updating package lists..."
 apt-get update
-apt-get install -y google-chrome-stable chromium-chromedriver
 
-# Start the Flask app with Gunicorn
+echo "Installing dependencies..."
+apt-get install -y wget unzip curl
+
+echo "Installing Google Chrome..."
+wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i /tmp/chrome.deb || apt-get -fy install  # Fix dependencies
+
+echo "Installing Chromedriver..."
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d'.' -f1)
+wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0.0/chromedriver_linux64.zip
+unzip /tmp/chromedriver.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
+
+echo "Starting Flask API..."
 gunicorn -w 1 -b 0.0.0.0:$PORT app:app
