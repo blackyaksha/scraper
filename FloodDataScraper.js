6c68f119b09f5f03f5c25e53673de7afd55f69fd
Emman <101398596+blackyaksha@users.noreply.github.com>
2025-04-03 09:19:08 +0800

    Update FloodDataScraper.js

--- FloodDataScraper.js
+++ FloodDataScraper.js
@@ -16,7 +16,15 @@ const SENSOR_CATEGORIES = {
         "QCPU", "Masambong", "Batasan Hills", "Ugong Norte", "Ramon Magsaysay HS",
         "UP Village", "Dona Imelda", "Kaligayahan", "Emilio Jacinto Sr HS", "Payatas ES",
         "Ramon Magsaysay Brgy Hall", "Phil-Am", "Holy Spirit", "Libis",
-        "South Triangle", "Nagkaisang Nayon", "Tandang Sora", "Talipapa",
+        "South Triangle", "Nagkaisang Nayon", "Tandang Sora", "Talipapa", 
+        "Balingasa High School", "Toro Hills Elementary School",
+        "Quezon City University San Francisco Campus", "Maharlika Brgy Hall", "Bagong Silangan Evacuation Center",
+        "Dona Juana Elementary School", "Quirino High School",
+        "Old Balara Elementary School", "Pansol Kaingin 1 Brgy Satellite Office",
+        "Jose P Laurel Senior High School", "Pinyahan Multipurose Hall", "Sikatuna Brgy Hall",
+        "Kalusugan Brgy Hall", "Laging Handa Barangay Hall", "Amoranto Sport Complex",
+        "Maligaya High School", "San Agustin Brgy Hall", "Jose Maria Panganiban Senior High School",
+        "North Fairview Elementary School", "Sauyo Elementary School", "New Era Brgy Hall", "Ismael Mathay Senior High School",
         "Brgy Fairview (REC)", "Brgy Baesa Hall", "Brgy N.S Amoranto Hall", "Brgy Valencia Hall"
     ],
     flood_sensors: [
