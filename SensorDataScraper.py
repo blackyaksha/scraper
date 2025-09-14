4c1925d6bcaa8c61354cf89c2a4259f82fac2ead
Emman <101398596+blackyaksha@users.noreply.github.com>
2025-09-14 17:55:17 +0800

    Update SensorDataScraper.py

--- SensorDataScraper.py
+++ SensorDataScraper.py
@@ -17,8 +17,8 @@ import os
 
 # Set all internal file reads/writes to be relative to this script
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SENSOR_DATA_FILE = os.path.join("/tmp", "sensor_data.json")
-CSV_FILE_PATH = os.path.join("/tmp", "sensor_data.csv")
+SENSOR_DATA_FILE = os.path.join(/tmp, "sensor_data.json")
+CSV_FILE_PATH = os.path.join(/tmp, "sensor_data.csv")
 
 # Configure logging
 logging.basicConfig(
@@ -48,18 +48,12 @@ app.add_middleware(
 )
 
 SENSOR_CATEGORIES = {
-"rain_gauge": [
+    "rain_gauge": [
         "QCPU", "Masambong", "Batasan Hills", "Ugong Norte", "Ramon Magsaysay HS",
         "UP Village", "Dona Imelda", "Kaligayahan", "Emilio Jacinto Sr HS", "Payatas ES",
         "Ramon Magsaysay Brgy Hall", "Phil-Am", "Holy Spirit", "Libis",
         "South Triangle", "Nagkaisang Nayon", "Tandang Sora", "Talipapa",
-        "Balingasa High School", "Toro Hills Elementary School", "Quezon City University San Francisco Campus", 
-        "Maharlika Brgy Hall", "Bagong Silangan Evacuation Center", "Dona Juana Elementary School",
-        "Quirino High School", "Old Balara Elementary School", "Pansol Kaingin 1 Brgy Satellite Office",
-        "Jose P Laurel Senior High School", "Pinyahan Multipurose Hall", "Sikatuna Brgy Hall",
-        "Kalusugan Brgy Hall", "Laging Handa Barangay Hall", "Amoranto Sport Complex", "Maligaya High School",
-        "San Agustin Brgy Hall", "Jose Maria Panganiban Senior High School", "North Fairview Elementary School",
-        "Sauyo Elementary School", "New Era Brgy Hall", "Ismael Mathay Senior High School"
+        "Brgy Fairview (REC)", "Brgy Baesa Hall", "Brgy N.S Amoranto Hall", "Brgy Valencia Hall"
     ],
     "flood_sensors": [
         "North Fairview", "Batasan-San Mateo", "Bahay Toro", "Sta Cruz", "San Bartolome"
